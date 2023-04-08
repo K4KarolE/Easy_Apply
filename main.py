4a412cd362fc48e764aa55d07286c632b0619a95
karoly.egyed@protonmail.com <karoly.egyed@protonmail.com>
2023-04-08 22:21:36 +0100

    save button, saving updated fields

--- main.py
+++ main.py
@@ -16,10 +16,14 @@ def path_json(name_json):
     return path_json
 
 
-def open_json(name_json):
+def open_db(name_json):
     f = open(path_json(name_json))
     json_dictionary = json.load(f)
     return json_dictionary
+
+def save_db(database):
+    with open('database.json', 'w') as f:
+        json.dump(database, f, indent=2)
 
 
 # BUTTONS
@@ -31,7 +35,7 @@ class Buttons:
         def create(self):
             return Button(window, 
                             height=1,
-                            width=2,
+                            width=5,
                             text = self.text,
                             command = self.command,
                             foreground=font_color,
@@ -39,18 +43,7 @@ class Buttons:
                             activeforeground=background_color,
                             activebackground=font_color,
                             font=(font_style, font_size))
-        # CELSIUS - FAHRENHEIT
-        def standard(self):
-            return self.configure(foreground=background_color,
-                            background="#505050",
-                            activeforeground=background_color,
-                            activebackground=background_color)
-        
-        def selected(self):
-            return self.configure(foreground=font_color,
-                            background=background_color,
-                            activeforeground=background_color,
-                            activebackground=font_color)
+
 # FIELDS
 class Fields:
     def __init__(self, width, height, background):
@@ -81,7 +74,7 @@ class Title:
                      background=background_color)
 
 # LOAD DB
-database = open_json("database.json")
+database = open_db("database.json")
 
 background_color = database['settings']['background_color']
 field_background_color = database['settings']['field_background_color']
@@ -101,6 +94,11 @@ window.geometry(f'{window_width}x{window_length}+%d+%d' % (screen_width/1.9, 0))
 window.resizable(0,0)   # locks the main window
 window.configure(background=background_color)
 
+# WINDOW ICON
+working_directory = os.path.dirname(__file__)
+path_icon = Path(working_directory, "skin", "icon.ico") 
+window.iconbitmap(path_icon)
+
 # COPY TO CLIPBOARD
 pyperclip.copy(database['contacts']['email'])
 
@@ -111,7 +109,7 @@ x_base_field = 35
 y_base=40
 y_base_right_side = window_width-300
 y_gap=40
-self = {}
+self = {}   # store the widgets for able to save the update
 
 def y_location(gap):
     location = y_base + 40 * gap
@@ -122,18 +120,23 @@ def create_text(text, x, y):
     self[text].place(x=x, y=y)
 
 
-def create_field(dic_name, width, height, x, y):
+def create_field(dic_name,width, height, x, y):     # for FULL NAME, INTRO
     self[dic_name] = Fields(width, height, field_background_color).create()
+    self[dic_name].insert(END,database[dic_name])
     self[dic_name].place(x=x, y=y)
-    if dic_name != 'contacts':
-        self[dic_name].insert(END,database[dic_name])
-    else:
-        self[dic_name].insert(END,database['contacts'][item])
 
-def create_field_plus(dic_name, field_name, width, height, x, y):
-    self[dic_name] = Fields(width, height, field_background_color).create()
-    self[dic_name].place(x=x, y=y)
-    self[dic_name].insert(END,database[dic_name][item][field_name])
+
+def create_contacts_field(contacts_type, width, height, x, y):
+    self[contacts_type] = Fields(width, height, field_background_color).create()
+    self[contacts_type].insert(END,database['contacts'][contacts_type])
+    self[contacts_type].place(x=x, y=y)
+
+
+def create_field_plus(dic_name, field_name, width, height, x, y):   # for EXPEREIENCE, EDUCATION
+    unique_name = f'{item}_{field_name}'
+    self[unique_name] = Fields(width, height, field_background_color).create()
+    self[unique_name].insert(END,database[dic_name][item][field_name])
+    self[unique_name].place(x=x, y=y)
     # SCROLLBAR - unnecessary, text is already scrollable
     # if field_name == 'description':
     #     scrollbar = ttk.Scrollbar(window, orient='vertical', command=self[dic_name].yview)
@@ -149,11 +152,12 @@ create_field('full_name', 22, 1, x_base_field, y_location(1))
 create_text('INTRO', x_base, y_location(2))
 create_field('intro', 80, 5, x_base_field, y_location(3))
 
+
 # CONTACTS
 create_text('CONTACTS', y_base_right_side, y_location(2))
 n=3
 for item in database['contacts']:
-    create_field('contacts', 26, 1, y_base_right_side+5, y_location(n))
+    create_contacts_field(item, 26, 1, y_base_right_side+5, y_location(n))
     n += 1
 
 # EXPEREIENCE
@@ -168,5 +172,24 @@ for item in database['experience']:
     create_field_plus('experience', "to", 8, 1, x_base_field+300, y_location(n+1))
     n += 6
 
+# SAVE BUTTON
+def save_update():
+    # FULL NAME
+    database['full_name'] = self['full_name'].get("1.0", "end-1c")
+    # INTRO
+    database['intro'] = self['intro'].get("1.0", "end-1c")
+    # CONTACTS
+    for contacts_type in database['contacts']:
+        database['contacts'][contacts_type] = self[contacts_type].get("1.0", "end-1c")
+    # EXPERIENCE
+    for item in database['experience']:
+        for field_name in database['experience'][item]:
+            unique_name = f'{item}_{field_name}'
+            database['experience'][item][field_name] = self[unique_name].get("1.0", "end-1c")
+
+    save_db(database)
+
+save_button = Buttons('SAVE', save_update).create()
+save_button.place(x=window_width-100, y=y_base)
 
 mainloop()
